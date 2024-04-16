61319924d518b2f32d9311f6d729100006279a98
unknown <Tayyab0112>
2024-04-16 15:16:47 +0530

    inserted boiler plate code

---
+++ script.js
@@ -0,0 +1,45 @@
+// $('#text p').textillate({ in: { effect: 'rollIn' } });
+ window.addEventListener('resize', function (){ 
+ 'use strict';
+ window.location.reload(); 
+})
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+    locoScroll.on('scroll', ScrollTrigger.update);
+    ScrollTrigger.scrollerProxy('#main', {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        },
+        pinType: document.querySelector('#main').style.transform ? 'transform' : 'fixed'
+    });
+    ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
+    ScrollTrigger.refresh();
+}
+init()
+
+document.querySelector('#main').addEventListener('mousemove',function(dets){
+    document.querySelector('circle').style.left = `${dets.clientX}px`
+    document.querySelector('circle').style.top = `${dets.clientY}px`
+})
+
+gsap.fromTo('child',{
+scrollTrigger:{
+    trigger:'parent',
+    scrub:true,
+    markers:true,
+    start:'top 50%',
+    end:'top 0%',
+},
+})
